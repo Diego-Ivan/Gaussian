517604bdfef2ff7b1707d85d8232e50fb2ff9d1e
Diego Iván <diegoivan.mae@gmail.com>
2023-06-20 15:55:16 -0600

    Add John Burkardt's asa239 C implementation + VAPI

---
+++ src/Calculus/asa239.h
@@ -0,0 +1,8 @@
+/*
+ * Original source: https://people.sc.fsu.edu/~jburkardt/c_src/asa239/asa239.h
+ * Distributed under the GNU LGPL license.
+ */
+double alnorm ( double x, int upper );
+void gamma_inc_values ( int *n_data, double *a, double *x, double *fx );
+double gammad ( double x, double p, int *ifault );
+double r8_min ( double x, double y );
